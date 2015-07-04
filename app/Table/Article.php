1c993a7369082d625bce6be20b22d6b21bccc040
syrez <vinkeey@gmail.com>
2015-07-04 13:03:11 +0200

    second commit

--- app/Table/Article.php
+++ app/Table/Article.php
@@ -3,14 +3,15 @@ namespace App\Table;
 
 class Article{
 
-    public  function __get($key){
-        var_dump('okkkk __get ');
+    public  function __get($key)
+    {
         $method = 'get'. ucfirst($key);
         $this->$key = $this->$method();
         return $this->$key;
     }
 
-    public function getUrl(){
+    public function getUrl()
+    {
 
         return 'index.php?p='. str_replace(__NAMESPACE__."\\",'',__CLASS__ ).'&id=' . $this->id;
 
@@ -21,5 +22,10 @@ class Article{
         $html = "<p>". substr($this->contenu,0, 100) ."... </p>";
         $html .= "<p><a href={$this->getURL()}>Voir la suite</a></p>";
         return $html;
+    }
+
+    public function getExtrait()
+    {
+        return "<p> $this->contenu </p>";
     }
 }
--- pages/home.php
+++ pages/home.php
@@ -4,13 +4,7 @@
     <h2><a href="<?= $post->Url ?>"> <?= $post->titre ?> </a></h2>
 
     <p><?= $post->Extrait ?></p>
-    <p><?= $post->Extrait ?></p>
-    <p><?= $post->Extrait ?></p>
-    <p><?= $post->Extrait ?></p>
-    <p><?= $post->Extrait ?></p>
-    <p><?= $post->Extrait ?></p>
-    <p><?= $post->Extrait ?></p>
-    <p><?= $post->Extrait ?></p>
+
 
 
 <?php endforeach; ?>
